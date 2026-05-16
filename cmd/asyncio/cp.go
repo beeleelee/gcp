b8ff75178dc43fc46707fd844e18c5bcfee3cb9b
beeleelee <beeleelee@outlook.com>
2026-05-16 14:42:49 +0800

    refine/code resuse: define function handle copy one file to host

--- cmd/asyncio/cp.go
+++ cmd/asyncio/cp.go
@@ -3,11 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
-	"os"
-	"sync"
-	"time"
 
-	"github.com/beeleelee/gcp/cmd/progressbar"
 	"github.com/urfave/cli/v2"
 )
 
@@ -40,72 +36,6 @@ var cpCmd = &cli.Command{
 		if src == "" || target == "" {
 			return errors.New("[usage] asyincio src target")
 		}
-		// open the src
-		sfd, err := os.Open(src)
-		if err != nil {
-			return
-		}
-		defer sfd.Close()
-		// read file meta data
-		sfinfo, err := sfd.Stat()
-		if err != nil {
-			return
-		}
-		batch := c.Int("batch")
-		cc := newClient(ctx, hostAddr, batch)
-
-		// touch target file
-		_, err = cc.Create(target, sfinfo.Size(), sfinfo.Mode())
-		if err != nil {
-			return
-		}
-		var wg sync.WaitGroup
-		var chunkSize, offset int64
-		chunkSize = c.Int64("chunk")
-		remainSize := sfinfo.Size()
-
-		concurrentCtl := make(chan struct{}, batch)
-		errChan := make(chan error, 0)
-		progressChan := make(chan int64, batch+1)
-		// print progress
-		go progressbar.Progress(ctx, sfinfo.Size(), progressChan, time.Now(), time.Millisecond*200)
-
-		for remainSize > 0 {
-			select {
-			case err = <-errChan:
-				break
-			default:
-			}
-			chus := chunkSize
-			if remainSize < chus {
-				chus = remainSize
-			}
-			wg.Add(1)
-			go func(off int64, size int64) {
-				defer wg.Done()
-				concurrentCtl <- struct{}{}
-				defer func() {
-					<-concurrentCtl
-				}()
-				buf := make([]byte, size)
-				_, err := sfd.ReadAt(buf, off)
-				if err != nil {
-					errChan <- err
-					return
-				}
-				_, err = cc.Write(target, off, buf)
-				if err != nil {
-					errChan <- err
-					return
-				}
-				progressChan <- size
-
-			}(offset, chus)
-			offset += chus
-			remainSize -= chus
-		}
-		wg.Wait()
-
-		return err
+		return cpOneFileToHost(ctx, hostAddr, src, target, c.Int64("chunk"), c.Int("batch"))
 	},
 }
